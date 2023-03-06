9a0d8b0b59c857d6ecd9aef0d8dd0f56ee30b5fb
BakaX <37313808+RuotongX@users.noreply.github.com>
2023-03-06 21:45:01 +1300

    Finished the UI part

--- src/FridgePage.js
+++ src/FridgePage.js
@@ -1,47 +1,140 @@
-import React from 'react'
-import { NavBar, SearchBar, List, Modal} from 'antd-mobile'
-import {  AddOutline } from 'antd-mobile-icons'
-import './FridgePage.css'
-
+import React from "react";
+import { NavBar, SearchBar, List, Modal, Calendar, Input } from "antd-mobile";
+import { AddOutline } from "antd-mobile-icons";
+import moment from "moment";
+import "./FridgePage.css";
 
 export default function FridgePage() {
-    const right =<AddOutline fontSize={30} onClick={handleAddFood}/>
-    const back = <div/>
-    function handleAddFood(){
-        Modal.alert({
-            content: '人在天边月上明',
-            onConfirm: () => {
-              console.log('Confirmed')
-            },
-          })
-    }
-    function handleEditFood(){
-        Modal.alert({
-            content: '人在天边月上明',
-            onConfirm: () => {
-              console.log('Confirmed')
-            },
-          })
-    }
-    const fakedata =[
-        {name:'苹果', date:'2023/3/1'},
-        {name:'猪肉',date:'2023/3/5'},
-        {name:'香蕉',date:'2023/2/28'},
-        {name:'猪肉',date:'2023/3/6'},
-    ]
-    return(
+  // var selectDate = "";
+  const right = <AddOutline fontSize={30} onClick={handleAddFood} />;
+  const currentDate = moment().format("YYYY-MM-DD");
+  const defaultSingle = new Date(currentDate);
+  const back = <div />;
+
+  function handleAddFood() {
+    Modal.show({
+      content: (
         <div>
-        <NavBar right ={right} backArrow={back} style={{'--height':'100px','--border-bottom':'1px #eee solid'}}>
-            <div>Fridge Content</div>
-            <SearchBar placeholder='害害，来啦' className="SearchBar"/>
-        </NavBar>
-        <List >
-            {fakedata.map(food => (
-                <List.Item key={food.name} description={food.date} onClick={handleEditFood}>
-                    {food.name}
-                </List.Item>
-            ))}
-        </List>
+        <Calendar
+          selectionMode="single"
+          defaultValue={defaultSingle}
+          onChange={(val) => {
+            console.log(val);
+          }}
+        >
+          {" "}
+        </Calendar>
+        <Input
+            placeholder="好吃的"
+            className="InputBar"
+            // value={value}
+            //   onChange={val => {
+            //     setValue(val)
+            //   }}
+          />
+          <Input
+            placeholder="有几个？"
+            className="InputBar"
+            // value={value}
+            //   onChange={val => {
+            //     setValue(val)
+            //   }}
+          />
+        </div>
+      ),
+      closeOnAction: true,
+      actions: [
+        {
+          key: "Add",
+          text: "添加",
+          primary: true,
+        },
+    ],
+      showCloseButton: true,
+    });
+  }
+
+  function handleEditFood() {
+    Modal.show({
+      content: (
+        <div>
+          <Calendar
+            selectionMode="single"
+            defaultValue={defaultSingle}
+            onChange={(val) => {
+              console.log(val);
+              // selectDate = val;
+            }}
+          >
+            {" "}
+          </Calendar>
+          <Input
+            placeholder="还剩几个捏？"
+            style ={{"border":"solid 1px #cfcfcf","borderRadius":"6px"}}
+            // value={value}
+            //   onChange={val => {
+            //     setValue(val)
+            //   }}
+          />
+        </div>
+      ),
+      closeOnAction: true,
+      actions: [
+        {
+          key: "decrease",
+          text: "吃掉一个",
+          primary: true,
+        },
+        {
+          key: "confirm",
+          text: "修改日期or数量",
+
+          //   onClick: handleEditDate(food,selectDate),
+        },
+        {
+          key: "delete",
+          text: "删除",
+        },
+      ],
+      showCloseButton: true,
+    });
+  }
+
+  // function handleEditDate(food,date){
+  //     let index = fakedata.findIndex((item)=>item.name === food)
+  //     fakedata[index].date = moment(date).format('YYYY-MM-DD')
+  // }
+
+  const fakedata = [
+    { name: "苹果", date: "2023/3/1", amout: 5 },
+    { name: "猪肉", date: "2023/3/5", amout: 7 },
+    { name: "香蕉", date: "2023/2/28", amout: 10 },
+    { name: "猪肉", date: "2023/3/6", amout: 4 },
+  ];
+  return (
+    <div>
+      <NavBar
+        right={right}
+        backArrow={back}
+        style={{ "--height": "100px", "--border-bottom": "1px #eee solid" }}
+      >
+        <div>冰箱里的宝藏</div>
+        <SearchBar placeholder="害害，来啦" className="SearchBar" />
+      </NavBar>
+      <List>
+        {fakedata.map(function (food, index) {
+          return (
+            <List.Item
+              key={index}
+              description={food.date}
+              extra={food.amout}
+              onClick={handleEditFood}
+            >
+              {food.name}
+            </List.Item>
+          );
+        })}
+      </List>
     </div>
-    );
+  );
 }
